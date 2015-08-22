5c77b1847fd845c0a84ff2c6ae72b6f29699e93d
Vivek Poonia <developer.shaurya@gmail.com>
2015-08-22 15:33:46 -0700

    removing unwanted files

--- project/try.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-import math as math
-
-x = np.array([1,2],dtype=float)
-list1 = np.array(range(1,10),dtype=float)
-print np.append(x,list1)
-#print gradAscent(rho,w,x,firstMomentEWMA,secondMomentEWMA,R)
