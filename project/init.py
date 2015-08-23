60a57b77f6f00f6ab92d791f5519e05f86eb92f0
Shaurya-Poonia <shauryapoonia@gmail.com>
2015-08-23 23:03:08 +0530

    initial commita

--- project/init.py
+++ project/init.py
@@ -8,8 +8,8 @@ import sys as sys
 
 def getData(stock):
     """ Returns apple data between start and end data"""
-    start = dt.datetime(1970,1,1)
-    end = dt.datetime(1995,1,1)
+    start = dt.datetime(2012,1,1)
+    end = dt.datetime(2014,1,1)
     apple = dataReader.DataReader(stock,'yahoo',start,end)
     return apple
 
@@ -23,10 +23,35 @@ def closingPriceData(stock):
     return closePriceList
 
 
-def gradAscent(rho,maPos,x,deltaDSR,R):
+
+def gradAscent(rho,maxPosition,friction,currentPos,deltaDSR,xt,x,wt,rt):
+    sign = (currentPos-xt[1])/abs(currentPos-xt[1])
+    
+    temp = sign*(1-currentPos*currentPos)
+    tempList1 = np.array([-1*friction*temp for a in xt],dtype=float)
+    temp = -1*temp*wt[1]*friction+rt+friction*sign
+    tempList2 = np.array([a*temp for a in x],dtype=float)
+    #print len(tempList1),len(tempList2)
+    tempList = np.array(tempList1+tempList2,dtype=float)
+    tempList = [maxPosition*deltaDSR*a*rho for a in tempList]
+    if(math.isnan(np.tanh(np.dot(wt,tempList)))):
+        sys.exit()
+    """print("******************************************")
+    print -1*temp*wt[1]*friction
+    print wt[1]
+    print sign
+    print temp
+    print tempList1
+    print tempList2
+
+    print("******************************************")"""
+    return np.array(tempList,dtype=float) 
+
+
+"""def gradAscent(rho,maPos,x,deltaDSR,R):
 	temp = rho*deltaDSR*maPos*R
         #print(rho,x,deltaDSR,R)
-	return [temp*i for i in x]
+	return [temp*i for i in x]"""
 
 
 if __name__ == '__main__':
@@ -41,6 +66,7 @@ if __name__ == '__main__':
     maxPosition = 1
     nFeatures = 10
     rho = 0.5
+    friction = 0.01
     totalData = len(apple)
     #calculating return per stock
     r = map(lambda a,b:a-b,apple[1:],apple[:-1])
@@ -52,12 +78,15 @@ if __name__ == '__main__':
     R=[0]
     R =np.array(R,dtype=float)
     #first position i am taking is full buy
-    F = np.array([1],dtype=float)
+    F = np.array([1,1],dtype=float)
 
-    #x is the feature vector
+    #x1 is the feature vector at time t-1
     x = []
-    #w is the parameter vector which will be tuned to maximize the profit
-    w = np.array([1,1],dtype=float)
+    #x is the feature vector at time t
+    xt = []
+    
+    #wt is the parameter vector which will be tuned to maximize the profit at time t
+    wt = np.array([1,1],dtype=float)
     #temperary variable for ith position
     F_t=np.array([],dtype=float)
 
@@ -65,7 +94,8 @@ if __name__ == '__main__':
     #these will be exactly nFeature+2 elements in the feature vector, nFeature number of returns and two other elements
     #we wont have this size of window since the start itself
     #so we have to slowly increase the window size
-    for t in range(1,nFeatures+1):
+    R = np.append(R,r[1]*F[0]*maxPosition)
+    for t in range(2,nFeatures):
         R = np.append(R,[r[t]*F[-1]*maxPosition])
         #calculating exponential weighted mean average for first and second moment of Rt
         secondMoment = np.array( [i*i for i in R],dtype=float)
@@ -76,23 +106,25 @@ if __name__ == '__main__':
         deltaDSR = secondMomentEWMA[-2]-firstMomentEWMA[-2]*R[-1]/ math.pow(secondMomentEWMA[-2]-math.pow(firstMomentEWMA[-2],2),1.5)
         #x here is feature vector
         #contains last n returns per stock, and t-1th position
-        x=np.append([1,F[-1]],np.array(R[-int(t):],dtype=float))
-        w = np.append(w,np.array(1,dtype=float))
+        x=np.append([1,F[-2]],np.array(R[-int(t):-1],dtype=float))
+        xt = np.append([1,F[-1]],np.array(R[-int(t-1):],dtype=float))
+        wt = np.append(wt,np.array(1,dtype=float))
         #call Gradient asecent with repect to w here
-        w1 = np.array(np.zeros(w.size),dtype=float)
-        if not t==1:
-            for num in range(100):
-                w1 = w+ gradAscent(rho,maxPosition,x,deltaDSR,R[-1]/F[-1])
-                w = w1
-        else:
-            pass
-        #print w,x
-        F_t = np.tanh(np.dot(w,x))
+        w1 = np.array(np.zeros(wt.size),dtype=float)
+        #print wt.size,x.size,xt.size
+    
+        F_t = 0.5
+        for num in range(100):
+            w1= wt+ gradAscent(rho,maxPosition,friction,F_t,deltaDSR,xt,x,wt,R[-1]/F[-1])
+            wt = w1
+            F_t = np.tanh(np.dot(wt,xt))
+            print wt[1]
+        if t==3:
+            sys.exit()
         F = np.append(F,F_t)
 
-    #sys.exit()
-    t = nFeatures+1
-    w = np.array(w,dtype=float)
+    t = nFeatures+2
+    wt = np.array(wt,dtype=float)
     while(t<totalData):
         R = np.append(R,[r[t]*F[-1]*maxPosition])
         #calculating exponential weighted mean average for first and second moment of Rt
@@ -103,19 +135,25 @@ if __name__ == '__main__':
 
         #x here is feature vector
         #contains last n returns per stock, and t-1th position
-        x=np.append([1,F[-1]],np.array(R[-int(nFeatures):],dtype=float))
+        x=np.append([1,F[-2]],np.array(R[-int(nFeatures+1):-1],dtype=float))
+        xt=np.append([1,F[-1]],np.array(R[-int(nFeatures):],dtype=float))
+        
         #no need to increase feature vector here since we have reached proper window size
         #w = np.append(w,np.zeros(1,dtype=float))
         #calculate gradient of differential sharp Ratio
         deltaDSR = secondMomentEWMA[-2]-firstMomentEWMA[-2]*R[-1]/ math.pow(secondMomentEWMA[-2]-math.pow(firstMomentEWMA[-2],2),1.5)
-
+        print x,xt
         #call Gradient asecent with repect to w here
         w1 = np.array(np.zeros(nFeatures+2),dtype=float)
-        for num in range(1000):
-            w1 = w+gradAscent(rho,maxPosition,x,deltaDSR,R[-1]/F[-1])
-            w = w1
-
-        F_t = np.tanh(np.dot(w,x))
+        F_t=0.5
+        for num in range(100):
+            w1= wt+ gradAscent(rho,maxPosition,friction,F_t,deltaDSR,xt,x,wt,R[-1]/F[-1])
+            wt = w1
+            F_t = np.tanh(np.dot(wt,xt))
+            print w1,F_t,t
+            if math.isnan(F_t):
+                sys.exit()
+            
         F = np.append(F,F_t)
         t = t+1
 
